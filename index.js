6abfa0c0a2f4b05af3ef725c0c29b03ecbe25467
Lang Kenney <Lang411@gmail.com>
2016-02-22 18:03:04 -0800

    JQuery for API called on button click

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-  if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(function(position) {
-$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+$(document).ready(function() {
+console.log('you loaded the page and JQuery Loaded');
+	$("#allButton").click(function (){
+		console.log('you clicked the All streamers button');
+		//var a = $("input[name=searchString]").val();
+		//console.log('This is your search string: '+a);
+		$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+			console.log(data);
+		});
+	});
+	$('#onlineButton').click(function (){
+		console.log('you clicked the Online button');
+		//var a = $("input[name=searchString]").val();
+		//console.log('This is your search string: '+a);
+		$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+			console.log(data);
+		});
+	});
+	$('#offlineButton').click(function (){
+		console.log('you clicked the Offline button');
+		//var a = $("input[name=searchString]").val();
+		//console.log('This is your search string: '+a);
+		$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+			console.log(data);
+		});
+	});
 });
-}
